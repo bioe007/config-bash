a07fb1d36c15f2fdb1c43821dd5ffd35aeeb3a62
Perry Hargrave <resixian@gmail.com>
2011-09-26 11:06:14 -0700

    add some alias

--- alias.sh
+++ alias.sh
@@ -1,19 +1,33 @@
+# ls
 alias l.='ls -lAdF ${H} ${LS_COLOUR} .*'    # list only hidden things
 alias la='ls -lAF ${H} ${LS_COLOUR}'        # list all
 alias ll='ls -lF ${H} ${LS_COLOUR}'         # list long
 alias ls='ls -F ${LS_COLOUR}'               # list with flags
 
-alias python=`which python2`
-alias gcc_w=__mgcc $@
+# packaging
 alias p="sudo pacman"
+alias c="sudo clyde"
 alias mp="makepkg"
 alias mpf="makepkg -f"
+
+# applications
 alias v="/usr/bin/vim"
 alias vi="/usr/bin/vim"
 alias :q="exit"
+alias vbxp2="VBoxSDL --nohostkeys r -vm xp2"
+alias pg="less"
+alias python=`which python2`
+alias tma="tmux attach || tmux"
+alias hstr='mplayer -demuxer ogg \
+           http://xertia.homelinux.org:8000/mpd && \
+           echo -e "\a" || echo -e "\a"'
+
+# directories
 alias cds="cd $SANDBOX"
+alias src="cd $SANDBOX"
 alias mv="mv -i"
 
+# grepping
 alias g='grep'
 alias gi='grep -i'
 alias gn='grep -n'
@@ -21,13 +35,11 @@ alias G='| grep'
 alias GI='| grep -i'
 alias GN='| grep -n'
 
-alias pg="less"
-# alias R="PAGER=\"`which less`\" `which R` --save --quiet"
-alias vbxp2="VBoxSDL --nohostkeys r -vm xp2"
-
+# find
 alias find_broken_links='find . -type l | \
                             (while read FN ; do \
                                 test -e "$FN" || ls -ld "$FN"; \
                             done)'
 
+# scm
 alias gst="git status"
